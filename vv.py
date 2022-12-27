8ca13a82e3310f276b66e7dbf917411168ccdfa8
USMAN-RAJPOOT <113978329+USMAN-RAJPOOT@users.noreply.github.com>
2022-12-27 20:54:12 +0500

    Add files via upload

---
+++ vv.py
@@ -0,0 +1,20 @@
+
+▄▄▄   ▌ ▐·▄▄▄  
+▀▄ █·▪█·█▌▀▄ █·
+▐▀▀▄ ▐█▐█•▐▀▀▄ 
+▐█•█▌ ███ ▐█•█▌
+.▀  ▀. ▀  .▀  ▀
+By : Putra-XD
+[?25l[!] Reverse nmbf.py will begin. Please wait...
+I don't know about Python version '3.10.8' yet.
+Python versions 3.9 and greater are not supported.
+I don't know about Python version '3.10.8' yet.
+Python versions 3.9 and greater are not supported.
+I don't know about Python version '3.10.8' yet.
+Python versions 3.9 and greater are not supported.
+I don't know about Python version '3.10.8' yet.
+Python versions 3.9 and greater are not supported.
+I don't know about Python version '3.10.8' yet.
+Python versions 3.9 and greater are not supported.
+I don't know about Python version '3.10.8' yet.
+Python versions 3.9 and greater are not supported.
